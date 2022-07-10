f736f59d7ef792468b9302a909b74252da3e0099
Miguel <sanchez.vc@hotmail.com>
2022-07-10 15:47:51 -0500

    Begin app search country

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+
+  termino : string = 'Hola mundo';
 
   constructor() { }
 
-  ngOnInit(): void {
+  buscar(){
+    console.log(this.termino);
   }
-
+  
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -9,7 +11,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     SidebarComponent,
