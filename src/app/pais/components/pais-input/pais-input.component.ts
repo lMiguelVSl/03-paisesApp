9b967f4c2a4fe08ae7510965f41b9f9ea50c5ed9
Miguel <sanchez.vc@hotmail.com>
2022-07-14 17:29:30 -0500

    Adding the obvservable and showing the value in console

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,21 +1,39 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html',
   styleUrls: ['./pais-input.component.css']
 })
-export class PaisInputComponent {
+export class PaisInputComponent implements OnInit {
 
-  @Output() onEnter : EventEmitter<string> = new EventEmitter();
+  @Output() onEnter    : EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce : EventEmitter<string> = new EventEmitter();
   @ViewChild('inputPais') inputPais !: ElementRef<HTMLInputElement>;
+
+  debouncer : Subject<string> = new Subject();
 
   termino : string = '';
 
   constructor() { }
 
+  ngOnInit() {
+    
+    this.debouncer.subscribe( value => {
+      console.log('debouncer: ', value);
+    });
+
+  }
+
   buscar() {
     this.onEnter.emit( this.termino );
     this.inputPais.nativeElement.value = '';
+  }
+
+  letterPress( ){
+
+    this.debouncer.next( this.termino );
+
   }
 }
